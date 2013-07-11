cf6501301e56d347503a85e788805fcda234af80
PACIFICA <MOOKBOOK13@Taruns-MacBook-Air.local>
2013-07-12 01:16:24 +1000

    looks goood

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,18 +5,14 @@ def intellinav
 			if @auth.is_admin
 				links << "<li>#{link_to('Show People', people_path)}</li>"
 			end
-			links << "<li>#{link_to('Edit', edit_person_path(@auth))}</li>"
-			links << "<li>#{link_to('Create Event', new_event_path)}</li>"
-			links << "<li>#{link_to('Create Organisation', new_organisation_path)}</li>"
-			links << "<li>#{link_to('Logout ' + @auth.name, login_path, :method => :delete, :confirm => 'Really?')}</li>"
-			links << "</li>#{link_to('Home', root_path)}</li>"
-
+			# links << "<li>#{link_to('Edit', edit_person_path(@auth))}</li>"
+			links << "<li>#{link_to('New Event', new_event_path)}</li>"
+			links << "<li>#{link_to('New Organisation', new_organisation_path)}</li>"
+			links << "<li>#{link_to('Logout ' + @auth.name + ' ' + @auth.surname, login_path, :method => :delete, :confirm => 'Really?')}</li>"
 		else
-			links << "<li>#{link_to('Create Account', new_person_path)}</li>"
+			links << "<li>#{link_to('New Account', new_person_path)}</li>"
       		links << "<li>#{link_to('Login', login_path)}</li>"
 		end
-
 		links
-
 	end
 end
